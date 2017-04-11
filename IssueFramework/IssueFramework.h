982e6aeb5e83ddf4a84a2c1e31ee658d369559fe
Ken Drew <kdrew@irobot.com>
2017-04-11 10:10:10 -0400

    Added IssueFramework

---
+++ IssueFramework/IssueFramework.h
@@ -0,0 +1,19 @@
+//
+//  IssueFramework.h
+//  IssueFramework
+//
+//  Created by Ken Drew on 4/11/17.
+//  Copyright © 2017 BigBadBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IssueFramework.
+FOUNDATION_EXPORT double IssueFrameworkVersionNumber;
+
+//! Project version string for IssueFramework.
+FOUNDATION_EXPORT const unsigned char IssueFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IssueFramework/PublicHeader.h>
+
+
